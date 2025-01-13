65357c4c458879336d07d3e7c93e158e8d7dfbe4
brundin <134782358+brundindev@users.noreply.github.com>
2025-01-13 18:42:08 +0100

    update

---
+++ src/main/java/es/fempa/acd/plataformacursosonline/PlataformacursosonlineApplication.java
@@ -0,0 +1,13 @@
+package es.fempa.acd.plataformacursosonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformacursosonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformacursosonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/fempa/acd/plataformacursosonline/PlataformacursosonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fempa.acd.plataformacursosonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformacursosonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
